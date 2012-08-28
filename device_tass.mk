d29b4a45c64c934ee7442f8d772311af95a8f85c
Dāvis Mālnieks <daavis.90@gmail.com>
2012-08-28 19:55:24 +0300

    initial commit

---
+++ device_tass.mk
@@ -0,0 +1,39 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Inherit vendor proprietary files
+$(call inherit-product, vendor/samsung/tass/tass-vendor.mk)
+
+include device/samsung/msm7x27-common/common.mk
+
+DEVICE_PACKAGE_OVERLAYS := device/samsung/tass/overlay
+
+## Audio
+PRODUCT_PACKAGES += \
+    audio.primary.tass \
+    audio_policy.tass
+
+## Camera
+PRODUCT_PACKAGES += \
+    camera.tass
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/tass/ramdisk/init.gt-s5570board.rc:root/init.gt-s5570board.rc \
+    device/samsung/tass/ramdisk/ueventd.gt-s5570board.rc:root/ueventd.gt-s5570board.rc \
+
+## LDPI assets
+PRODUCT_AAPT_PREF_CONFIG := ldpi
+
+
